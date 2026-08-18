00e31ba66fc63893e822c37d17988fb4205733eb
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-18 19:07:05 +0900

    uv.config.js

--- public/uv.config.js
+++ public/uv.config.js
@@ -1,1 +1,10 @@
-
+self.__uv$config = {
+    prefix: '/sw/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.1.0/dist/uv.handler.js',
+    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.1.0/dist/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/sw.js',
+};
