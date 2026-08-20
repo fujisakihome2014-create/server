48bd41cc3211f77f46b05f356d203ec245216adc
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-21 07:05:25 +0900

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,28 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv.config.js');
-importScripts('/uv/uv.sw.js');
-
-const uv = new UVServiceWorker();
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        uv.fetch(event)
-    );
-});
+self.__uv$config = {
+    prefix: '/sw/',
+    bare: '', // EpoxyとBare-Muxを使う場合は空にしておきます
+    encodeUrl: (str) => {
+        if (!str) return '';
+        try {
+            return encodeURIComponent(str.toString().split('').map((char, ind) => 
+                ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
+            ).join(''));
+        } catch (err) {
+            return str;
+        }
+    },
+    decodeUrl: (str) => {
+        if (!str) return '';
+        try {
+            return decodeURIComponent(str).split('').map((char, ind) => 
+                ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
+            ).join('');
+        } catch (err) {
+            return str;
+        }
+    },
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
