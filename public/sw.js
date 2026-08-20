a43644742277554f3f7d22b2f952efd708323d8f
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-21 07:49:12 +0900

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,6 @@ importScripts('/uv/uv.sw.js');
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    // 条件分岐で弾かずに、すべてのリクエストを Ultraviolet に処理させることでサイトの開き残しを無くす
     event.respondWith(
         uv.fetch(event)
     );
