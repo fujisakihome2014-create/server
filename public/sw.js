a7ce356d8f75e2b185c1aa20146c46a2bca9f872
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-21 20:27:30 +0900

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,8 @@ importScripts('/uv/uv.sw.js');
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        uv.fetch(event)
-    );
+    // リクエストがプロキシ経由（/sw/ など）のものであれば Ultraviolet で処理する
+    if (event.request.url.includes(__uv$config.prefix)) {
+        event.respondWith(uv.fetch(event));
+    }
 });
