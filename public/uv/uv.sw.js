17fbe9f490943c5dc1b60706bc45846f926c9f83
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-27 10:58:11 +0900

    uv.sw.js

---
+++ public/uv/uv.sw.js
@@ -0,0 +1,8 @@
+/* Ultraviolet Service Worker Implementation */
+class UVWorker extends UVServiceWorker {
+    constructor() {
+        super();
+    }
+}
+// グローバルにインスタンスを紐付け
+const uvWorker = new UVWorker();
