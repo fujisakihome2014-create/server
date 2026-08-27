8e233243bee2726567508428152342eee8019e8a
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-27 10:55:59 +0900

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,15 +5,7 @@ importScripts('/uv/uv.sw.js');
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    // MessagePortを必要とする処理でエラーが出ないよう、uv.fetchを安全にラップ
-    try {
-        event.respondWith(
-            uv.fetch(event).catch((err) => {
-                console.error("UV Fetch Error:", err);
-                return fetch(event.request);
-            })
-        );
-    } catch (e) {
-        event.respondWith(fetch(event.request));
+    if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+        event.respondWith(uv.fetch(event));
     }
 });
