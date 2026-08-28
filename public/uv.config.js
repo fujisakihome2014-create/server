36e1316b99cef4a001a211288657ad1d84c4c5f0
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-29 06:46:23 +0900

    uv.config.js

--- public/uv.config.js
+++ public/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
-    config: '/uv.config.js',
+    config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
