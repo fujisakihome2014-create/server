91be0cd3f5761d9cdcdf41d83662aaee72b845b4
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-28 09:23:10 +0900

    uv.handler.js

--- public/uv/uv.handler.js
+++ public/uv/uv.handler.js
@@ -1,6 +1,4 @@
-/* Ultraviolet Client Handler */
 if (typeof window !== 'undefined') {
-    // ページ内のリンクやフェッチをプロキシ経由に書き換えるためのフック
     const rawFetch = window.fetch;
     window.fetch = async function(input, init) {
         let url = typeof input === 'string' ? input : input.url;
