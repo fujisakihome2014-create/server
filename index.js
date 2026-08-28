6fa851484786b2a8c01e4a90dc6c905be46c95af
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-28 09:23:34 +0900

    index.js

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 
-// Bareサーバーを /bare/ で作成
 const bareServer = createBareServer('/bare/');
 
-// 静的ファイルの配信（ここでヘッダー競合を防ぐ）
 app.use(express.static(path.join(__dirname, 'public'), {
   setHeaders: (res) => {
     if (!res.headersSent) {
@@ -24,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public'), {
   }
 }));
 
-// HTTP リクエストのルーティング
 server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
@@ -33,7 +30,6 @@ server.on('request', (req, res) => {
   }
 });
 
-// WebSocket のルーティング
 server.on('upgrade', (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
