cba73cbb18a9d34439b8631699ded2983f283831
fujisakihome2014-create <fujisakihome2014@gmail.com>
2026-08-29 06:53:44 +0900

    Delete public/uv/uv.sw.js

--- public/uv/uv.sw.js
+++
@@ -1,8 +0,0 @@
-/* Ultraviolet Service Worker Implementation */
-class UVWorker extends UVServiceWorker {
-    constructor() {
-        super();
-    }
-}
-// グローバルにインスタンスを紐付け
-const uvWorker = new UVWorker();
